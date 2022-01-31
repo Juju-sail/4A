527c30c9819f3f9b475271d321cd34a5c5636301
Juju-sail <77498912+Juju-sail@users.noreply.github.com>
2022-01-31 19:45:17 +0100

    weekend fin janvier

--- Apprentissage Modèles dynamiques/seance6.m
+++ Apprentissage Modèles dynamiques/seance6.m
@@ -1,13 +1,13 @@
-tk = [0:0.1:0.9]';
+tk = [0:15]';
 
 % vecteur de sortie
-y = [0.2 0.21 0.23 0.26 0.28 0.32 0.35 0.4 0.44 0.49]';
+y = [0 1.34 2.05 2.43 2.82 2.90 2.98 2.91 3.02 2.93 2.94 1.59 0.85 0.4 0.27 0.13]';
 
 % mat regression
-phi = [1/2*tk.^2 tk ones(length(y),1)]
+
 
 % vecteur de paramètres
-theta = inv(phi'*phi)*phi'*y
+theta = inv([a1 b0])
 
 % calcul sortie avec theta estimé
 ybis = phi * theta
