aec1f5531dee2ba3500eb7e99909ebe1c1bb4425
Juju-sail <77498912+Juju-sail@users.noreply.github.com>
2022-02-01 17:51:43 +0100

    TP models dynamiques

--- Mécatronique/TP_PasAPas/cycleAttendu/cycleAttendu.ino
+++ Mécatronique/TP_PasAPas/cycleAttendu/cycleAttendu.ino
@@ -1,9 +1,10 @@
-#define STEP 5
-
+#define STEP 2
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Debut programme");
+  pinMode (STEP, OUTPUT);
+
 // On utilse le timer 1, capable de coder sur 16bits
 
 // Registres :
@@ -13,9 +14,9 @@ void setup() {
   bitClear(TCCR1B, WGM13);  
 
 // test diviser frequence quartz par 256 : (011 dans tccr1B)
-  bitSet(TCCR1B, CS12);
-  bitClear(TCCR1B, CS11);
-  bitClear(TCCR1B, CS10);
+  bitClear(TCCR1B, CS12);
+  bitSet(TCCR1B, CS11);
+  bitSet(TCCR1B, CS10);
 
   TCNT1 = 0;
 
@@ -23,22 +24,23 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-    Maintien(0,4);
+    //Maintien(0,7);
     Accelerer(0,30000,25);
-    Maintien(30000, 8);
-    Accelerer(30000,60000,50);
-    Maintien(60000, 32);
-    Decelerer(60000,30000,15);
-    Maintien(30000,16);
-    Decelerer(30000,0,25);   
+    //Maintien(30000, 480);
+    Accelerer(30000,60000,25);
+    //Maintien(60000, 960);
+    Decelerer(60000,30000,25);
+    //Maintien(30000,480);
+    Decelerer(30000,0,25);
+    //Maintien(0,240);
 }
 // Méthode permettant de maintenir le moteur à une même frequence en fonction d'un nb de pas coeficié
 void Maintien(unsigned int freq, int tours){
   for(int i=0; i<tours; i++){
-    TCNT1 = freq;
     Impulsion();
+    TCNT1 = freq;
     bitSet(TIFR1,TOV1);
-    //Serial.println(freq);
+    Serial.println(freq);
   }      
 }
 
@@ -47,10 +49,10 @@ void Accelerer(unsigned int depart,unsigned int arrivee,int pente){
   int increment = (arrivee - depart)/pente;
   while(depart+increment<arrivee){
     if (bitRead(TIFR1, TOV1) == 1){
-      TCNT1 = depart;
       Impulsion();
+      TCNT1 = depart;
       bitSet(TIFR1,TOV1);
-      //Serial.println(depart);
+      Serial.println(depart);
       depart += increment;
     }
   }
@@ -61,10 +63,10 @@ void Decelerer(unsigned int depart,unsigned int arrivee, int pente){
   int increment = (depart - arrivee)/pente;
   while(arrivee+increment<depart){
     if (bitRead(TIFR1, TOV1) == 1){
-      TCNT1 = depart;
       Impulsion();
+      TCNT1 = depart;
       bitSet(TIFR1,TOV1);
-      //Serial.println(depart);
+      Serial.println(depart);
       depart -= increment;
     }
   }
