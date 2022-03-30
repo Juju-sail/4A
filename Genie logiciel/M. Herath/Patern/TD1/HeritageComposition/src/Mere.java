667dea7eec9b8e1a733896c64d77743ae2f27af1
Juliette <77498912+Juju-sail@users.noreply.github.com>
2022-03-30 15:37:32 +0200

    exo1

--- Genie logiciel/M. Herath/Patern/TD1/HeritageComposition/src/Mere.java
+++ Genie logiciel/M. Herath/Patern/TD1/HeritageComposition/src/Mere.java
@@ -1,4 +1,14 @@
 
 public class Mere {
-
+	public void compter() {
+		for (int i = 0; i<10; i++) {
+			System.out.print(i+1 + " ");
+		}
+		System.out.println(" ");
+	}
+	
+	/*public static void main(String[] args) {
+		Mere ma = new Mere();
+		ma.compter();
+	}*/
 }
--- Genie logiciel/M. Herath/Patern/TD1/HeritageComposition/src/Pere.java
+++ Genie logiciel/M. Herath/Patern/TD1/HeritageComposition/src/Pere.java
@@ -1,4 +1,17 @@
 
 public class Pere {
+	public String nom;
+	public String prenom;
+	
+	public String getNom() {
+		return nom;
+	}
 
+	public Pere(String nom) {
+		super();
+		this.nom = nom;
+	}
+	
+	
+	
 }
